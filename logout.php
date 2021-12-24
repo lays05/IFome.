69401c1ed4a9e64230ce7c9cb9d58d6ba338d679
lays05 <95623859+lays05@users.noreply.github.com>
2021-12-24 16:17:42 -0300

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+// Inicializa a sessão
+session_start();
+ 
+// Remova todas as variáveis ​​de sessão
+$_SESSION = array();
+ 
+// Destrua a sessão.
+session_destroy();
+ 
+// Redirecionar para a página de login
+header("location: login.php");
+exit;
+?>
